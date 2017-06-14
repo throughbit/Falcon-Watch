fd44fceca66b8298e3ecddf00519bbcd3891a9eb
Mohammed Roshan <bitroshan7@gmail.com>
2017-06-14 23:52:57 +0530

    Falcon watch v1

---
+++ watch-inrf.js
@@ -0,0 +1,26 @@
+var Web3 = require('web3');
+var child_process = require('child_process');
+var web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+var inrf_addr = '0x23Bf0408eC1DEE24c71614BfDFaf06c7B98eAdfF';
+var inrf_abi = JSON.parse('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"},{"name":"mintedAmount","type":"uint256"}],"name":"mintToken","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"},{"name":"centralMinter","type":"address"}],"payable":false,"type":"constructor"},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]');
+var tokenContract = web3.eth.contract(inrf_abi).at(inrf_addr);
+
+
+var events = tokenContract.allEvents({fromBlock:  3855071, toBlock: 'pending'});
+events.watch(function(error, event){
+  if (!error)
+    {
+      var txs_json = JSON.stringify(event);
+      var txs_parse = JSON.parse(txs_json);
+      console.log(txs_json);
+      var tx_hash = txs_parse.transactionHash;
+      var args =txs_parse.args;
+      var from_addr = args.from;
+      var to_addr = args.to;
+      var amount = args.value;
+
+    }
+  else console.log(JSON.stringify(error));
+});
